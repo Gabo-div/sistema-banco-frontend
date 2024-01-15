40b84ab510859b73f4fcfcf925405afa958e38ba
jjavCode <jhordammv@gmail.com>
2024-01-15 11:52:40 -0400

    create register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -8,17 +8,28 @@ export default function Home() {
           <h1 className="text-4xl font-bold text-center mb-8">LOGO</h1>
 
           <form className="flex flex-col">
-            <label>
+            <label className="mt-6">
+              <span className="ml-1 mb-2 block">Name</span>
+              <input type="text" className="w-full px-3 py-3 rounded-xl bg-gray-100" placeholder="Pedrito Gonzales" />
+            </label>
+            <label className="mt-6">
               <span className="ml-1 mb-2 block">Correo</span>
               <input type="text" className="w-full px-3 py-3 rounded-xl bg-gray-100" placeholder="ejemplo@ejemplo.com" />
             </label>
-
+            <label className="mt-6">
+            <span className="ml-1 mb-2 block">Fecha de nacimiento</span>
+            <input type="date" className="w-full px-3 py-3 rounded-xl bg-gray-100"/>
+            </label>
             <label className="mt-6">
               <span className="ml-1 mb-2 block">Contraseña</span>
               <input type="password" className="w-full px-3 py-3 rounded-xl bg-gray-100" placeholder="*******" />
             </label>
+            <label className="mt-6">
+              <span className="ml-1 mb-2 block">Repetir contraseña</span>
+              <input type="password" className="w-full px-3 py-3 rounded-xl bg-gray-100" placeholder="*******" />
+            </label>
 
-            <button type="submit" className="mt-8 w-full px-3 py-3 rounded-xl bg-blue-500 text-white">Iniciar sesión</button>
+            <button type="submit" className="mt-8 w-full px-3 py-3 rounded-xl bg-blue-500 text-white">Registrar</button>
           </form>
           <div className="mt-6">
             <p>¿Ya tienes una cuenta? <a href="#" className="text-blue-500">Log in</a></p>
